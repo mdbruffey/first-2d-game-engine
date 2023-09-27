ecac7f915e76d0e88c6a1727336e7470dc013a06
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-09-27 16:09:37 -0500

    Fix bouncing on the constraint
    Reworked position and velocity calculations.

--- src/engine.js
+++ src/engine.js
@@ -13,9 +13,9 @@ class Engine{
             "radius" : radius,
             "position" : new Vector(x, y),
             "previousPosition" : new Vector(x, y),
-            "velocity" : new Vector(0,0),
+            "velocity" : new Vector(0, 0),
             "acceleration" : new Vector(0, 0),
-            "restitution" : 1,
+            "restitution" : .95,
             "update" : bodyUpdate,
             "color" : "#FF0000",
             ...options
@@ -36,21 +36,20 @@ class Engine{
         for (let body of this.bodies){
             let diff = body.position.y+body.radius - this.height;
             if (diff > 0){
-                let v1y = Math.sqrt(body.velocity.y**2 - 2*this.GRAVITY*diff);
-                let t1 = (v1y-body.previousVelocity.y)/this.GRAVITY;
-                
-                body.position = new Vector(body.position.x, body.position.y - body.radius).sum(body.velocity.mult(t1-dt));
-                body.velocity = body.velocity.mult(-1).sum(body.acceleration.mult(dt-t1));
+                let root = body.velocity.y**2 - 2*this.GRAVITY*diff;
+                if (root < 0 ) root = 0;
+                let v1 = Math.sqrt(root);
+                body.position = new Vector(body.position.x, this.height-body.radius);
+                body.velocity = new Vector(body.velocity.x, -v1).mult(body.restitution);
+                console.log(body.velocity);
             }
         }
     }
 }
 
 var bodyUpdate = function(dt){
-    //let velocity = this.position.sum(this.previousPosition.mult(-1));
     this.previousPosition = this.position;
-    this.previousVelocity = this.velocity;
-    this.position = this.position.sum(this.velocity.mult(dt));
+    this.position = this.position.sum(this.velocity.mult(dt)).sum(this.acceleration.mult(0.5*dt*dt));
     this.velocity = this.velocity.sum(this.acceleration.mult(dt));
     this.acceleration = new Vector(0,0);
 };
