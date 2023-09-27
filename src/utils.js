926857307b630e84cf2bdbca16e997bae7894ac9
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-09-26 19:07:49 -0500

    Implements a Vector class
    For 2D vectors; methods include length(), mult() and sum().

---
+++ src/utils.js
@@ -0,0 +1,29 @@
+export default class Vector{
+    #x;
+    #y;
+
+    constructor(x, y){
+        this.#x = x;
+        this.#y = y;
+    }
+
+    length(){
+        return Math.hypot(this.#x, this.#y)
+    }
+
+    mult(num){
+        return new Vector(this.#x * num, this.#y * num);
+    }
+
+    sum(vec){
+        return new Vector(this.#x + vec.x, this.#y + vec.y);
+    }
+
+    get x() {
+        return this.#x;
+    }
+    
+    get y() {
+        return this.#y;
+    }
+}
