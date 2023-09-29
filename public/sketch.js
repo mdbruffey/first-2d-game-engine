02470de334738604835234899a4fe84296dd8bbd
Matthew Bruffey <25539194+mdbruffey@users.noreply.github.com>
2023-09-28 22:44:31 -0500

    Add function to display total Energy
    Calculates kinetic and potential energy for each non-fixed body.

--- public/sketch.js
+++ public/sketch.js
@@ -4,6 +4,9 @@ const DEFAULT_RADIUS = 15;
 
 
 let engine = new Engine(WIDTH, HEIGHT);
+let energyDisplay = document.createElement('div');
+energyDisplay.setAttribute('class', 'energyDisplay')
+document.body.appendChild(energyDisplay);
 
 function setup() {
     noStroke();
@@ -26,5 +29,16 @@ function draw(){
 
     delta = (Date.now() - lastUpdate)/1000;
     engine.step(delta);
+    calculateEnergy();
     lastUpdate = Date.now();
+}
+
+function calculateEnergy(){
+    let energy = 0;
+    for(let body of engine.bodies){
+        if(body.fixed) continue;
+        energy += .5*body.mass*body.velocity.length()**2;
+        energy += body.mass*981*(HEIGHT - body.position.y);
+    }
+    energyDisplay.textContent = parseInt(energy);
 }
